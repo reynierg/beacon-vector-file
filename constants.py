260948f5e269460a4aaebdb88e11655e127c9c79
Reynier Garcia <reynierg@protonmail.com>
2021-01-07 22:33:52 +0100

    Define constants

---
+++ constants.py
@@ -0,0 +1,9 @@
+import logging
+
+LOG_LEVEL = logging.INFO
+INFO_MESSAGE_FORMAT = "%(message)s"
+DEBUG_MESSAGE_FORMAT = "%(levelname)s|%(filename)s:%(lineno)s|%(asctime)s|%(message)s"
+DATE_FORMAT = "%H:%M:%S"
+DEFAULT_DBM_ANT_VALUE = -135
+ANTENNA_IDS = [201, 202, 203, 204, 205, 206]
+RESULTS_FILE_NAME = "results.json"
