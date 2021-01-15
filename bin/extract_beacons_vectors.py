344122504ebecb6bbc11976589c52abf57b73862
Reynier Garcia <reynierg@protonmail.com>
2021-01-15 19:29:24 +0100

    Move entrypoint script

---
+++ bin/extract_beacons_vectors.py
@@ -0,0 +1,14 @@
+import inspect
+import os
+import sys
+
+if __name__ == "__main__":
+    current_dir = os.path\
+        .dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    parent_dir = os.path.dirname(current_dir)
+
+    if parent_dir not in sys.path:
+        sys.path.insert(0, parent_dir)
+
+    from src import main
+    main.main()
